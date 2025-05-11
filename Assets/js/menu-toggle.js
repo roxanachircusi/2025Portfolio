37f2d477ccac86a31ceb3cffaff08380b68c387c
roxanachircusi <118202242+roxanachircusi@users.noreply.github.com>
2025-05-11 18:05:24 +0200

    Rimosso .DS_Store da tutte le cartelle e aggiornato .gitignore

--- Assets/js/menu-toggle.js
+++
@@ -1,9 +0,0 @@
-// menu-toggle.js
-document.addEventListener('DOMContentLoaded', function () {
-  const toggleButton = document.querySelector('.central-menu-toggle');
-  const menu = document.querySelector('.central-menu');
-
-  toggleButton.addEventListener('click', function () {
-    menu.classList.toggle('show');
-  });
-});
